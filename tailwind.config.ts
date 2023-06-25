6d30264e980d239242d0381b3f17eab21bbcf84f
Samuel <samuelvvelzen@icloud.com>
2023-06-25 16:43:05 +0200

    add icons and fontfamily to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,25 @@
 import type { Config } from "tailwindcss";
+import defaultTheme from "tailwindcss/defaultTheme";
+
 
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary": "#001F54", 
+        "secondary": "#4392F1", 
+        "dark": "#1A1A1A", 
+        "darkgrey": "#738290", 
+        "lightgrey": "#F7F6F9", 
+        "light": "#FFFFFF",
+
+        "danger": "#FF0000",
+      },
+      fontFamily: {
+        'sans': ['"Open sans"', ...defaultTheme.fontFamily.sans],
+      } 
+    }
   },
   plugins: [],
 } satisfies Config;
