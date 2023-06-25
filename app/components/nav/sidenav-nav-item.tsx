129e1d524c909be6f398473226c9c1e3260f8d29
Samuel <samuelvvelzen@icloud.com>
2023-06-25 17:23:46 +0200

    create nav components

---
+++ app/components/nav/sidenav-nav-item.tsx
@@ -0,0 +1,44 @@
+import { NavLink } from "@remix-run/react";
+import { cloneElement, MouseEventHandler } from "react";
+
+type SidenavItemProps = {
+  className?: string;
+  icon: JSX.Element;
+  label: string;
+  path: string;
+  onClick?: MouseEventHandler<HTMLAnchorElement>;
+};
+
+export function SidenavNavItem({
+  className = "",
+  icon,
+  label,
+  path,
+  onClick,
+}: SidenavItemProps) {
+  function modify(child: JSX.Element, isActive: boolean) {
+    return cloneElement(child, {
+      className: `${child.props.className} ${isActive && "stroke-2"}`,
+    });
+  }
+
+  return (
+    <NavLink
+      to={path}
+      className={({ isActive }) =>
+        `${className} flex items-center rounded-3xl px-5 py-3 hover:underline ${
+          isActive && "bg-lightgrey font-bold"
+        }`
+      }
+      onClick={onClick}
+      end
+    >
+      {({ isActive, isPending }) => (
+        <>
+          {modify(icon, isActive)}
+          <span>{label}</span>
+        </>
+      )}
+    </NavLink>
+  );
+}
