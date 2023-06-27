b31cbdde4f486ed66c1e2c0d4757d80dc53810c7
Samuel <samuelvvelzen@icloud.com>
2023-06-27 21:28:01 +0200

    create dashboard header

---
+++ app/modules/dashboard/dashboard-header.tsx
@@ -0,0 +1,37 @@
+import { IBreadCrumbConfig } from "@Financely/UI/breadcrumb/breadcrumb";
+import { BreadCrumbs } from "@Financely/UI/breadcrumb/breadcrumbs";
+import { Container } from "@Financely/UI/container";
+import Title from "@Financely/UI/typography/title";
+import { HomeIcon } from "@heroicons/react/24/outline";
+import { routes } from "~/config/routes";
+import { createRoute } from "~/util/nav";
+
+type DashboardHeaderProps = {
+  className?: string;
+  titleText: string;
+  breadCrumbs?: IBreadCrumbConfig[];
+  rightChild?: React.ReactNode;
+};
+
+export function DashboardHeader({
+  className = "",
+  titleText,
+  breadCrumbs = [],
+  rightChild,
+}: DashboardHeaderProps) {
+  const homeRoute = {
+    path: createRoute([routes.dashboard.root]),
+    child: <HomeIcon className="inline-block h-5 w-5" />,
+  };
+
+  return (
+    <Container className={`${className} grid grid-cols-2`}>
+      <BreadCrumbs breadCrumbs={[homeRoute, ...breadCrumbs]} />
+      <Title text={titleText} className="col-start-1"></Title>
+
+      <span className="col-start-2 row-span-2 row-start-1 self-center justify-self-end">
+        {rightChild}
+      </span>
+    </Container>
+  );
+}
