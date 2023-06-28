fd3c04882d3291fd4b44ed703c0e11825ceeba03
Samuel <samuelvvelzen@icloud.com>
2023-06-29 00:16:37 +0200

    use generic input

--- app/components/forms/input/textarea-input.tsx
+++ app/components/forms/input/textarea-input.tsx
@@ -7,12 +7,12 @@ export function TextareaInput({ labelText }: TextareaInputProps) {
     <div className="relative">
       <textarea
         id="floating_outlined"
-        className="border-1 peer block w-full appearance-none rounded-lg border-gray-300 bg-transparent px-2.5 pb-2.5 pt-4 text-sm text-gray-900 focus:text-gray-900 focus:outline-none focus:ring-0"
+        className={`peer block w-full appearance-none rounded-full border border-darkgrey bg-transparent px-4 pb-2.5 pt-4 text-sm text-dark focus:text-dark focus:outline-none focus:ring-0`}
         placeholder=" "
       />
       <label
         htmlFor="floating_outlined"
-        className="absolute left-1 top-2 z-10 origin-[0] -translate-y-4 scale-75 transform bg-white px-2 text-sm text-gray-500 duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-2 peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-2 peer-focus:text-gray-900"
+        className={`absolute left-[1.2rem] top-[0.3rem] z-10 origin-[0] -translate-y-4 scale-75 transform rounded-full bg-light px-1 text-sm text-darkgrey duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-[0.3rem] peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-1 peer-focus:text-darkgrey`}
       >
         {labelText}
       </label>
