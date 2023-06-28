f80c539f52fac9c9e38905a5f2a9ba8d3080b390
Samuel <samuelvvelzen@icloud.com>
2023-06-29 00:20:19 +0200

    use the correct border-radius

--- app/components/forms/input/input.tsx
+++ app/components/forms/input/input.tsx
@@ -10,12 +10,12 @@ export function Input({ labelText, type, className }: InputProps) {
       <input
         type={type}
         id="floating_outlined"
-        className={`${className} peer block w-full appearance-none rounded-full border border-darkgrey bg-transparent px-4 pb-2.5 pt-4 text-sm text-dark focus:text-dark focus:outline-none focus:ring-0`}
+        className={`${className} peer block w-full appearance-none rounded-3xl border border-darkgrey bg-transparent px-4 pb-2.5 pt-4 text-sm text-dark focus:text-dark focus:outline-none focus:ring-0`}
         placeholder=" "
       />
       <label
         htmlFor="floating_outlined"
-        className={`absolute left-[1.2rem] top-[0.3rem] z-10 origin-[0] -translate-y-4 scale-75 transform rounded-full bg-light px-1 text-sm text-darkgrey duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-[0.3rem] peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-1 peer-focus:text-darkgrey`}
+        className={`absolute left-[1.2rem] top-[0.3rem] z-10 origin-[0] -translate-y-4 scale-75 transform rounded-3xl bg-light px-1 text-sm text-darkgrey duration-300 peer-placeholder-shown:top-1/2 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:scale-100 peer-focus:top-[0.3rem] peer-focus:-translate-y-4 peer-focus:scale-75 peer-focus:px-1 peer-focus:text-darkgrey`}
       >
         {labelText}
       </label>
