4de8e4cd6d16718d262ede19e3d73d80503e8b95
Samuel <samuelvvelzen@icloud.com>
2023-06-29 13:07:50 +0200

    create text component

--- app/components/typography/index.ts
+++ app/components/typography/index.ts
@@ -1,4 +1,5 @@
 export * from "./heading";
 export * from "./sub-text";
 export * from "./sub-title";
+export * from "./text";
 export * from "./title";
---
+++ app/components/typography/text.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+
+type TextProps = {
+  className?: string;
+  text: string;
+  shouldTruncate?: boolean;
+  maxSize?: number;
+};
+
+export function Text({
+  className = "",
+  text,
+  shouldTruncate = false,
+  maxSize = 50,
+}: TextProps) {
+  const [isTruncated, setIsTruncated] = useState(true);
+  const [truncatedText, setTruncatedText] = useState(
+    shouldTruncate ? text.substring(0, maxSize) : text
+  );
+
+  useEffect(() => {
+    const truncatedText = isTruncated ? text.substring(0, maxSize) : text;
+    setTruncatedText(truncatedText);
+  }, [isTruncated]);
+
+  return (
+    <div>
+      <p className={`${className} whitespace-pre-line`}>
+        {truncatedText}
+        {isTruncated && truncatedText.length < text.length && "..."}
+      </p>
+      <button
+        className="text-sm text-darkgrey"
+        onClick={() => setIsTruncated(!isTruncated)}
+      >
+        {isTruncated ? "Read more" : "Read less"}
+      </button>
+    </div>
+  );
+}
