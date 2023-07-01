95852a7afc57a154360773d246f5b86a6a23f012
Samuel <samuelvvelzen@icloud.com>
2023-07-01 12:34:24 +0200

    create CreateTransactionViewModel

---
+++ app/modules/transaction/types/CreateTransactionViewModel.ts
@@ -0,0 +1,13 @@
+import { TransactionTypeEnum } from "../enums";
+
+export type CreateTransactionViewModel = {
+  type?: TransactionTypeEnum;
+  amount?: number;
+  name?: string;
+  description?: string | null;
+  payDate?: Date;
+};
+
+export function createTransactionViewModel(): CreateTransactionViewModel {
+  return {};
+}
