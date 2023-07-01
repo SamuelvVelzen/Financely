11f88dd01baa2fe16cbecfe7a0236e929f77debc
Samuel <samuelvvelzen@icloud.com>
2023-07-01 17:18:36 +0200

    export amount component

--- app/components/typography/index.ts
+++ app/components/typography/index.ts
@@ -2,4 +2,6 @@ export * from "./heading";
 export * from "./sub-text";
 export * from "./sub-title";
 export * from "./text";
+export * from "./amount";
+export * from "./text";
 export * from "./title";
