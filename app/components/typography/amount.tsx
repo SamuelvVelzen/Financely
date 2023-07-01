c9568deb6a8f6b2f2ecee2c8fd9553877838804f
Samuel <samuelvvelzen@icloud.com>
2023-07-01 17:29:12 +0200

    improve showing amount

--- app/components/typography/amount.tsx
+++ app/components/typography/amount.tsx
@@ -3,10 +3,7 @@ type AmountProps = {
 };
 
 export function Amount({ amount }: AmountProps) {
-  const formatter = new Intl.NumberFormat(undefined, {
-    style: "currency",
-    currency: "USD",
-  });
+  const formatter = new Intl.NumberFormat(undefined, {});
 
   return <p>{formatter.format(parseFloat(amount))}</p>;
 }
