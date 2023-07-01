780938649d48e0b07ffbe23e005323fdadb71d15
Samuel <samuelvvelzen@icloud.com>
2023-07-01 14:24:35 +0200

    cleanup code

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -1,35 +1,12 @@
-import { createExpense } from "@Financely/Data/transaction";
 import { DashboardSidenav } from "@Financely/Module/dashboard";
-import type { ActionArgs, LoaderArgs, V2_MetaFunction } from "@remix-run/node";
+import type { LoaderArgs, V2_MetaFunction } from "@remix-run/node";
 import { Outlet } from "@remix-run/react";
-import { requireUserId } from "~/session.server";
 import { validateUserSession } from "../util";
 
 export const meta: V2_MetaFunction = () => [{ title: "Dashboard" }];
 
 export const loader = async ({ params, request }: LoaderArgs) => {
   return await validateUserSession(request);
-};
-
-export const action = async ({ request }: ActionArgs) => {
-  const userId = await requireUserId(request);
-
-  const formData = await request.formData();
-
-  const amount = formData.get("amount");
-  const name = formData.get("name");
-  const description = formData.get("description");
-  const payDate = formData.get("payDate");
-
-  const x = await createExpense({
-    amount: 2342.51,
-    name: "Test 1",
-    description: "Test 3 beschrijving",
-    payDate: new Date(),
-    userId,
-  });
-
-  return x;
 };
 
 export default function DashboardPage() {
