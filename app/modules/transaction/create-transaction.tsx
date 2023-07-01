52b932a91a11b663e17918f2ee782002c4f7ccc1
Samuel <samuelvvelzen@icloud.com>
2023-07-01 12:33:57 +0200

    create create-transactions list

---
+++ app/modules/transaction/create-transaction.tsx
@@ -0,0 +1,104 @@
+import {
+  Button,
+  DeleteButton,
+  EditButton,
+  OutlineButton,
+  ThemeColorEnum,
+} from "@Financely/UI/button";
+import {
+  DateInput,
+  IOption,
+  NumberInput,
+  SelectInput,
+  TextareaInput,
+  TextInput,
+} from "@Financely/UI/forms";
+import { useState } from "react";
+import { TransactionTypeEnum } from "./enums";
+import { CreateTransactionViewModel } from "./types";
+
+type CreateTransactionProps = {
+  data: CreateTransactionViewModel;
+  showEditMode?: boolean;
+};
+
+export function CreateTransaction({
+  data,
+  showEditMode = false,
+}: CreateTransactionProps) {
+  const [isEditMode, setIsEditMode] = useState(showEditMode);
+  const [showDetails, setShowDetails] = useState(false);
+
+  const options: IOption[] = [
+    { value: TransactionTypeEnum.income, label: "Income" },
+    { value: TransactionTypeEnum.expense, label: "Expense" },
+  ];
+
+  if (!isEditMode) {
+    return (
+      <div className="group flex items-center justify-between p-4">
+        <p>{data.name}</p>
+
+        <div className="flex ">
+          <p>{data.amount}</p>
+
+          <EditButton
+            onClick={() => setIsEditMode(true)}
+            className="invisible ml-2 group-hover:visible"
+          />
+          <DeleteButton className="invisible ml-2 group-hover:visible" />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="p-4">
+      <div className="mb-2 flex gap-4">
+        <SelectInput
+          name="type"
+          labelText={"Type"}
+          className="w-1/6"
+          options={options}
+        />
+
+        <NumberInput
+          name="amount"
+          labelText={"Amount"}
+          step={0.01}
+          min={0}
+          className="w-1/6"
+        />
+        <TextInput name="name" labelText={"Name"} className="w-3/6" />
+        <DateInput name="date" labelText={"Date"} className="w-1/6" />
+      </div>
+
+      <p
+        onClick={() => setShowDetails(!showDetails)}
+        className="mb-2 text-sm text-darkgrey underline"
+      >
+        {showDetails ? "Show" : "Hide"} advanced data
+      </p>
+
+      {showDetails && (
+        <div>
+          <TextareaInput name="description" labelText={"Description"} />
+        </div>
+      )}
+
+      <div className="flex justify-end">
+        <OutlineButton
+          theme={ThemeColorEnum.primary}
+          onClick={() => {
+            setIsEditMode(false);
+          }}
+        >
+          Cancel
+        </OutlineButton>
+        <Button theme={ThemeColorEnum.primary} className="ml-2">
+          Opslaan
+        </Button>
+      </div>
+    </div>
+  );
+}
---
+++ app/modules/transaction/create-transactions.tsx
@@ -0,0 +1,56 @@
+import { Button, ThemeColorEnum } from "@Financely/UI/button";
+import { useEffect, useState } from "react";
+import { CreateTransaction } from "./create-transaction";
+import {
+  createTransactionViewModel,
+  CreateTransactionViewModel,
+} from "./types";
+
+type TransactionData = {
+  data: CreateTransactionViewModel;
+  showEditMode: boolean;
+};
+
+type CreateTransactionsProps = {};
+
+export function CreateTransactions({}: CreateTransactionsProps) {
+  const [transactionData, setTransactionData] = useState<TransactionData[]>([]);
+
+  useEffect(() => {
+    createTransaction();
+  }, []);
+
+  function createTransaction(): void {
+    const transaction = createTransactionViewModel();
+    setTransactionData([
+      ...transactionData,
+      { data: transaction, showEditMode: true },
+    ]);
+  }
+
+  return (
+    <>
+      {transactionData.length > 0 && (
+        <div className="mb-4 divide-y divide-darkgrey rounded-3xl border border-darkgrey">
+          {transactionData.map((d, index) => (
+            <CreateTransaction
+              key={index}
+              data={d.data}
+              showEditMode={d.showEditMode}
+            />
+          ))}
+        </div>
+      )}
+
+      <Button
+        theme={ThemeColorEnum.primary}
+        className="mx-auto block"
+        onClick={() => {
+          createTransaction();
+        }}
+      >
+        Create transaction
+      </Button>
+    </>
+  );
+}
